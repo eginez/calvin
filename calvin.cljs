e43b0cf8582867f1ee9aa6d1c13c3207f6a9afab
Esteban <eginez@gmail.com>
2016-11-27 23:45:41 -0800

    Adding suport for repl with classpath

--- calvin.cljs
+++ calvin.cljs
@@ -3,6 +3,7 @@
 (require-macros '[cljs.core.async.macros :refer [go]])
 (require '[clojure.string :as strg]
          '[cljs.nodejs :as nodejs]
+         '[cljs.tools.cli :refer [parse-opts]]
          '[cljs.core.async :refer [put! take! chan <! >!] :as async] 
          '[eginez.huckleberry.core :as hb]
          '[cljs.reader :as reader])
@@ -10,6 +11,7 @@
 
 (def fs (nodejs/require "fs"))
 (def npath (nodejs/require "path"))
+(def nchild (nodejs/require "child_process"))
 (def nproc (nodejs/require "process"))
 (def argv (.-argv nproc))
 
@@ -33,25 +35,37 @@
       ret))
 
 
-(defn main [path retrieve]
-  (let [options (-> (find-file path)
+(defn resolve-classpath [path-to-project]
+  (let [options (-> (find-file path-to-project)
                     (find-lein-dependencies))
         deps (:dependencies options)]
-    (do 
       (go (let [dp (<!(hb/resolve-dependencies 
-                                      :coordinates deps
-                                      :local-repo (.resolve npath "./tmp/repo/")
-                                      :retrieve retrieve))]
+                        :coordinates deps
+                        :local-repo (.resolve npath "./tmp/repo/")
+                        :retrieve true))]
+            (strg/join ":" (map hb/dep->path dp))))))
 
-            (if retrieve
-              (println "classpath is: " (strg/join ":" (map hb/dep->path dp)))
-              (println "dependency graph " (second dp))))))))
+(defn run-repl []
+  (go
+    (let [cwd (.cwd nproc)
+          classpath (<! (resolve-classpath cwd))
+          args ["-c" classpath]
+          lumoProc (.spawn nchild "lumo" (clj->js args))]
+
+      (do 
+        (.setEncoding (.-stdin lumoProc) "utf-8")
+        (.pipe (.-stdin nproc) (.-stdin lumoProc))
+        (.pipe (.-stdout lumoProc) (.-stdout nproc))))))
 
 
+
+(def cli-options [["-h" "--help"]])
 ;Pass the a directory with a project file in it and it'll fetch the dependencies
-(let [pth (drop 6 argv)
-      rslv (.resolve npath (first pth))]
-  (main rslv (= "true" (strg/lower-case (last pth)))))
+(let [args (drop 6 argv)
+      {:keys [options arguments errors summ]} (parse-opts args cli-options :in-order true)]
+    (case (first arguments)
+      nil (run-repl)))
+  ;(main rslv (= "true" (strg/lower-case (last pth)))))
 
 
         
