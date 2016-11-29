ec714ee96e5eeaa2fff1a483a9d085fb9459600d
Esteban <eginez@gmail.com>
2016-11-28 22:57:04 -0800

    creating a lein project

---
+++ project.clj
@@ -0,0 +1,46 @@
+(defproject calvin "0.1.0"
+  :description "minimalistic clojurescript only build tool"
+  :url "https://github.com/eginez/calvin"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.293"]
+                 [org.clojure/tools.cli "0.3.5"]
+                 [org.clojars.eginez/huckleberry "0.2.0"]
+                 [andare "0.4.0"]]
+  :source-paths ["src/main/clojure" "target/classes"]
+  :test-paths ["src/test/clojure"]
+  :clean-targets ["out" "release" "target"]
+  :target-path "target"
+  :plugins [[lein-npm "0.6.1"]
+            [lein-cljsbuild "1.1.3"]]
+  :npm {
+        :dependencies [[source-map-support "0.4.0"]
+                       [xml2js "0.4.17"]
+                       [request "2.74.0"]]
+        }
+  :cljsbuild {
+              :builds [
+                       {:id "dev"
+                        :source-paths ["src/main/clojure"]
+                        :figwheel true
+                        :compiler {
+                                   :main eginez.calvin.core
+                                   :output-to "out/dev/calvin.js"
+                                   :target :nodejs
+                                   :output-dir "out/dev"
+                                   :optimizations :none
+                                   :parallel-build true
+                                   :source-map true}}
+                       {:id "test"
+                        :source-paths[ "src/main/clojure" "src/test/clojure"]
+                        :compiler {
+                                   :main eginez.huckleberry.test-runner
+                                   :output-to "out/test/test.js"
+                                   :target :nodejs
+                                   :output-dir "out/test"
+                                   :optimizations :none
+                                   :parallel-build true
+                                   :source-map true}
+                        }
+                       ]})
---
+++ src/main/clojure/eginez/calvin/core.cljs
@@ -0,0 +1,77 @@
+(ns eginez.calvin.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [clojure.string :as strg]
+         [cljs.nodejs :as nodejs]
+         [cljs.tools.cli :refer [parse-opts]]
+         [cljs.core.async :refer [put! take! chan <! >!] :as async]
+         [eginez.huckleberry.core :as hb]
+         [cljs.reader :as reader]))
+
+
+(def fs (nodejs/require "fs"))
+(def npath (nodejs/require "path"))
+(def nchild (nodejs/require "child_process"))
+(def nproc (nodejs/require "process"))
+(def argv (.-argv nproc))
+
+(defn find-file [fpath]
+  (let [files (.readdirSync fs fpath)
+        fname (-> (filter #(strg/includes? % "project.clj") files)
+                (first))]
+      (.join npath fpath fname)))
+
+
+(defn load-content [file]
+  (-> (.readFileSync fs file) .toString))
+
+(defn find-lein-dependencies [lein-project-file]
+  (let [content (load-content lein-project-file)
+        rcontent (reader/read-string content)
+        [_ name version & opts] rcontent
+        lopts (partition 2 opts)
+        mapopts (map #(assoc {} (first %) (second %)) lopts)
+        ret (reduce merge mapopts)]
+      ret))
+
+
+(defn resolve-classpath [path-to-project]
+  (let [options (-> (find-file path-to-project)
+                    (find-lein-dependencies))
+        deps (:dependencies options)]
+      (go (let [dp (<!(hb/resolve-dependencies 
+                        :coordinates deps
+                        :local-repo (:local hb/default-repos)
+                        :retrieve true))]
+            (strg/join ":" (map hb/dep->path dp))))))
+
+(defn build-cmd-for-platform [platform classpath]
+  (case platform
+    "lumo"    ["lumo" ["-c" classpath]]
+    "planck"  ["planck" ["-c" classpath]]))
+
+(defn run-repl [platform]
+  (go
+    (let [cwd (.cwd nproc)
+          classpath (<! (resolve-classpath cwd))
+          [bin args] (build-cmd-for-platform platform classpath)
+          proc (.spawn nchild bin (clj->js args) (clj->js {:stdio [0 1 2]}))]
+      proc)))
+
+(def cli-options [["-h" "--help"]
+                  ["-p" "--platform PLATFORM" "Either planck or lumo"
+                   :default "lumo"]])
+
+(defn -main[& args]
+  (let [{:keys [options arguments errors summ]}
+        (parse-opts args cli-options :in-order true)
+        platform (:platform options)]
+    (case (first arguments)
+      nil (run-repl platform))))
+
+(nodejs/enable-util-print!)
+(set! *main-cli-fn* -main)
+
+
+        
+
+
